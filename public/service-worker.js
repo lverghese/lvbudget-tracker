8798afa2aa3f3c46f55dd0a1e76ebf661b346fe1
lverghese <loganverghese@gmail.com>
2021-10-27 17:56:27 -0400

    installed service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -15,4 +15,14 @@ const FILES_TO_CACHE = [
     './icons/icon-128x128.png',
     './icons/icon-96x96.png',
     './icons/icon-72x72.png'
-  ];
+];
+
+// Cache resources
+self.addEventListener('install', function (e) {
+    e.waitUntil(
+      caches.open(CACHE_NAME).then(function (cache) {
+        console.log('installing cache : ' + CACHE_NAME)
+        return cache.addAll(FILES_TO_CACHE)
+      })
+    )
+  })
